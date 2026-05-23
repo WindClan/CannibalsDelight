72e96036ff315af2e55a156ebf4d01497e814690
WindClan <monstertoast2@gmail.com>
2026-05-23 18:28:32 -0400

    update to 1.21.1

--- src/main/java/com/alignedcookie88/dahmersdelight/DahmersDelightMod.java
+++ src/main/java/com/alignedcookie88/dahmersdelight/DahmersDelightMod.java
@@ -3,14 +3,14 @@ package com.alignedcookie88.dahmersdelight;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerEntityCombatEvents;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.component.type.FoodComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.FoodComponent;
+
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
@@ -29,50 +29,44 @@ public class DahmersDelightMod implements ModInitializer {
 	public static final String modId = "dahmersdelight";
     public static final Logger LOGGER = LoggerFactory.getLogger(modId);
 
-	public static final Item RAW_HUMAN_MEAT_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item RAW_HUMAN_MEAT_ITEM = new Item(new Item.Settings().food(
             new FoodComponent.Builder()
-					.hunger(2)
+					.nutrition(2)
 					.saturationModifier(0.5F)
-					.meat()
 					.statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*30), 0.25F)
 					.statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20*30), 0.25F)
 					.build()
 	));
-	public static final Item COOKED_HUMAN_MEAT_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item COOKED_HUMAN_MEAT_ITEM = new Item(new Item.Settings().food(
 			new FoodComponent.Builder()
-					.hunger(5)
+					.nutrition(5)
 					.saturationModifier(0.75F)
-					.meat()
 					.build()
 	));
-	public static final Item MINCED_HUMAN_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item MINCED_HUMAN_ITEM = new Item(new Item.Settings().food(
 			new FoodComponent.Builder()
-					.hunger(1)
+					.nutrition(1)
 					.saturationModifier(0.5F)
-					.meat()
 					.statusEffect(new StatusEffectInstance(StatusEffects.POISON, 20*30), 0.25F)
 					.statusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 20*30), 0.25F)
 					.build()
 	));
-	public static final Item HUMAN_PATTY_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item HUMAN_PATTY_ITEM = new Item(new Item.Settings().food(
 			new FoodComponent.Builder()
-					.hunger(2)
+					.nutrition(2)
 					.saturationModifier(0.75F)
-					.meat()
 					.build()
 	));
-	public static final Item HUMAN_BURGER_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item HUMAN_BURGER_ITEM = new Item(new Item.Settings().food(
 			new FoodComponent.Builder()
-					.hunger(15)
+					.nutrition(15)
 					.saturationModifier(0.9F)
-					.meat()
 					.build()
 	));
-	public static final Item PASTA_WITH_HUMAN_MEATBALLS_ITEM = new Item(new FabricItemSettings().food(
+	public static final Item PASTA_WITH_HUMAN_MEATBALLS_ITEM = new Item(new Item.Settings().food(
 			new FoodComponent.Builder()
-					.hunger(20)
+					.nutrition(20)
 					.saturationModifier(1.0F)
-					.meat()
 					.build()
 	));
 	public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder()
@@ -88,21 +82,21 @@ public class DahmersDelightMod implements ModInitializer {
 			})
 			.build();
 
-	public static final TagKey<Item> HUMAN_MEAT_KNIVES_ITAG = TagKey.of(RegistryKeys.ITEM, new Identifier(modId, "human_meat_knives"));
-	public static final TagKey<EntityType<?>> HUMAN_ENTITIES_ETAG = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(modId, "human_entities"));
+	public static final TagKey<Item> HUMAN_MEAT_KNIVES_ITAG = TagKey.of(RegistryKeys.ITEM, Identifier.of(modId, "human_meat_knives"));
+	public static final TagKey<EntityType<?>> HUMAN_ENTITIES_ETAG = TagKey.of(RegistryKeys.ENTITY_TYPE, Identifier.of(modId, "human_entities"));
 
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Registering items");
-		Registry.register(Registries.ITEM, new Identifier(modId, "raw_human_meat"), RAW_HUMAN_MEAT_ITEM);
-		Registry.register(Registries.ITEM, new Identifier(modId, "cooked_human_meat"), COOKED_HUMAN_MEAT_ITEM);
-		Registry.register(Registries.ITEM, new Identifier(modId, "minced_human"), MINCED_HUMAN_ITEM);
-		Registry.register(Registries.ITEM, new Identifier(modId, "human_patty"), HUMAN_PATTY_ITEM);
-		Registry.register(Registries.ITEM, new Identifier(modId, "human_burger"), HUMAN_BURGER_ITEM);
-		Registry.register(Registries.ITEM, new Identifier(modId, "pasta_with_human_meatballs"), PASTA_WITH_HUMAN_MEATBALLS_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "raw_human_meat"), RAW_HUMAN_MEAT_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "cooked_human_meat"), COOKED_HUMAN_MEAT_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "minced_human"), MINCED_HUMAN_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "human_patty"), HUMAN_PATTY_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "human_burger"), HUMAN_BURGER_ITEM);
+		Registry.register(Registries.ITEM, Identifier.of(modId, "pasta_with_human_meatballs"), PASTA_WITH_HUMAN_MEATBALLS_ITEM);
 		LOGGER.info("Registered.");
 		LOGGER.info("Registering item groups");
-		Registry.register(Registries.ITEM_GROUP, new Identifier(modId, "dahmersdelight"), ITEM_GROUP);
+		Registry.register(Registries.ITEM_GROUP, Identifier.of(modId, "dahmersdelight"), ITEM_GROUP);
 		LOGGER.info("Registered.");
 
 
